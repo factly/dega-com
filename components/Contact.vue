1fd1c6187134571f48a4894ee935f4a27c84112f
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-16 19:59:06 -0600

    contact form chagnes

--- components/Contact.vue
+++ components/Contact.vue
@@ -10,7 +10,7 @@
         </h4>
       </div>
       <br>
-      <form name="contact" method="POST" data-netlify="true" action="/confirm">
+      <form name="contact" method="POST" data-netlify="true" action="https://degafactcheck.com/confirm">
         <div class="columns">
           <div class="column is-6 is-offset-3">
             <div class="box">
