e5eb9e6f959d5d27e8b0a5d9ad42169062fa8d97
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-16 19:53:08 -0600

    Added a page for contact us confirmation

---
+++ pages/confirm.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero is-fullheight is-default is-centered is-link">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h2 class="subtitle is-4 is-marginless">
+          Your message has been submitted. Someone from the team will reach out to you soon. Thank you!
+        </h2>
+        <br>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script>
+export default {
+  props: {}
+}
+</script>
