88c7886e24c5a76f57fb3f94b75ae53b136f12e6
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-16 21:04:34 -0600

    contact form changes - 6

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <hero />
-    <contact />
+    <section class="hero is-fullheight is-default is-centered is-link">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <contact />
+        </div>
+      </div>
+      <div class="hero-foot">
+        <div class="container">
+          <p class="has-text-centered">
+            <a href="#contact" class="has-text-warning">Write to us if you are interested to try Dega for your fact check initiatives.</a>
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Hero from '~/components/Hero'
 import Contact from '~/components/Contact'
 
 export default {
   name: 'ContactUs',
 
   components: {
-    Hero,
     Contact
   }
 }
