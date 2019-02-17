29552570f7c40f01496b2526b737368cb4753eb8
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-17 08:05:18 -0600

    contact form changes - 13

--- components/Contact.vue
+++ components/Contact.vue
@@ -18,15 +18,37 @@
           <input class="input" type="text" placeholder="full name" name="fullName">
         </div>
       </div>
-      <p>
-        <label>Your Email: <input type="email" name="email"></label>
-      </p>
-      <p>
-        <label>Your Phone: <input type="phone" name="phone"></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message" /></label>
-      </p>
+      <div class="field">
+        <label class="label">Organisation</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="organisation name" name="organisation">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control has-icons-left">
+          <input class="input" type="email" placeholder="valid email address" name="email">
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope" />
+          </span>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Phone</label>
+        <div class="control has-icons-left">
+          <input class="input" type="text" placeholder="include the country code" name="phone">
+          <span class="icon is-small is-left">
+            <i class="fas fa-phone" />
+          </span>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="please be as descriptive as possible" name="message" />
+        </div>
+      </div>
       <p>
         <button type="submit">
           Send
