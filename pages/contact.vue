faa4ce9d59a16dafb6c3daa51f37a1c20741937f
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-16 20:54:53 -0600

    contact form changes - 5

---
+++ pages/contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <hero />
+    <contact />
+  </div>
+</template>
+
+<script>
+import Hero from '~/components/Hero'
+import Contact from '~/components/Contact'
+
+export default {
+  name: 'ContactUs',
+
+  components: {
+    Hero,
+    Contact
+  }
+}
+</script>
