11b3790b1fd14db3bc450197bbd5027041c7991b
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-17 07:35:59 -0600

    contact form changes - 10

--- components/Contact.vue
+++ components/Contact.vue
@@ -10,63 +10,6 @@
         </h4>
       </div>
       <br>
-      <!--<form name="contact" method="POST" data-netlify="true" action="/confirm">
-        <div class="columns">
-          <div class="column is-6 is-offset-3">
-            <div class="box">
-              <div class="field">
-                <label class="label">Name</label>
-                <div class="control">
-                  <input class="input" type="text" placeholder="full name" name="fullName">
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Organisation</label>
-                <div class="control">
-                  <input class="input" type="text" placeholder="organisation name" name="organisation">
-                </div>
-              </div>
-
-              <div class="field">
-                <label class="label">Email</label>
-                <div class="control has-icons-left">
-                  <input class="input" type="email" placeholder="valid email address" name="email">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-envelope" />
-                  </span>
-                </div>
-              </div>
-
-              <div class="field">
-                <label class="label">Phone</label>
-                <div class="control has-icons-left">
-                  <input class="input" type="text" placeholder="include the country code" name="phone">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-phone" />
-                  </span>
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Message</label>
-                <div class="control">
-                  <textarea class="textarea" placeholder="please be as descriptive as possible" name="message" />
-                </div>
-              </div>
-              &lt;!&ndash;<div data-netlify-recaptcha="true" />&ndash;&gt;
-              <div class="field is-grouped has-text-centered">
-                <div class="control">
-                  <button class="button is-link is-medium" type="submit">
-                    <span class="icon">
-                      <i class="fas fa-envelope" />
-                    </span>
-                    <span>Submit</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </form>-->
     </div>
     <form name="contact" method="POST" data-netlify="true">
       <p>
