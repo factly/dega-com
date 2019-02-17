0713ea856ddd67efe3b87f5bb3d7e33d0272e5e6
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-17 08:29:09 -0600

    contact form changes - 15

--- components/Contact.vue
+++ components/Contact.vue
@@ -13,53 +13,51 @@
     </div>
 
     <form name="contact" method="POST" data-netlify="true">
-      <div class="columns">
-        <div class="column is-6 is-offset-3">
-          <div class="field">
-            <label class="label has-text-white">Name</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="full name" name="fullName">
-            </div>
+      <div class="column is-6 is-offset-3">
+        <div class="field">
+          <label class="label has-text-white">Name</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="full name" name="fullName">
           </div>
-          <div class="field">
-            <label class="label has-text-white">Organisation</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="organisation name" name="organisation">
-            </div>
+        </div>
+        <div class="field">
+          <label class="label has-text-white">Organisation</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="organisation name" name="organisation">
           </div>
-          <div class="field">
-            <label class="label has-text-white">Email</label>
-            <div class="control has-icons-left">
-              <input class="input" type="email" placeholder="valid email address" name="email">
-              <span class="icon is-small is-left">
+        </div>
+        <div class="field">
+          <label class="label has-text-white">Email</label>
+          <div class="control has-icons-left">
+            <input class="input" type="email" placeholder="valid email address" name="email">
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope" />
+            </span>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label has-text-white">Phone</label>
+          <div class="control has-icons-left">
+            <input class="input" type="text" placeholder="include the country code" name="phone">
+            <span class="icon is-small is-left">
+              <i class="fas fa-phone" />
+            </span>
+          </div>
+        </div>
+        <div class="field has-text-white">
+          <label class="label has-text-white">Message</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="please be as descriptive as possible" name="message" />
+          </div>
+        </div>
+        <div class="field is-grouped has-text-centered">
+          <div class="control">
+            <button class="button is-warning is-medium" type="submit">
+              <span class="icon">
                 <i class="fas fa-envelope" />
               </span>
-            </div>
-          </div>
-          <div class="field">
-            <label class="label has-text-white">Phone</label>
-            <div class="control has-icons-left">
-              <input class="input" type="text" placeholder="include the country code" name="phone">
-              <span class="icon is-small is-left">
-                <i class="fas fa-phone" />
-              </span>
-            </div>
-          </div>
-          <div class="field has-text-white">
-            <label class="label has-text-white">Message</label>
-            <div class="control">
-              <textarea class="textarea" placeholder="please be as descriptive as possible" name="message" />
-            </div>
-          </div>
-          <div class="field is-grouped has-text-centered">
-            <div class="control">
-              <button class="button is-warning is-medium" type="submit">
-                <span class="icon">
-                  <i class="fas fa-envelope" />
-                </span>
-                <span>Submit</span>
-              </button>
-            </div>
+              <span>Submit</span>
+            </button>
           </div>
         </div>
       </div>
