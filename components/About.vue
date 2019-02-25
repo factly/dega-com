a759800f17027b6086f52e5bcd3a384f99ce3883
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-02-24 18:14:37 -0600

    Added favicon.png

--- components/About.vue
+++ components/About.vue
@@ -22,15 +22,23 @@
     </div>
     <div style="background-color: #EFF3F4" class="has-background-soundcloud">
       <div class="section intro column is-8 is-offset-2 has-text-centered">
-        <h2 class="title is-4">Let’s understand it further</h2><br>
-        <p class="has-text-left">Now, we have different checks that a fact checked article has to go through. Dega just brings all of those standards together so you don’t have to go to waste time in manually checking for them or going to different places for enforcing these standards including the ‘Claim Review’ schema.</p><br>
-        <p class="has-text-left">When you publish a fact-check article on Dega, the output is complaint with the ‘Claim Review’ schema as well as the standards of the IFCN.</p><br>
+        <h2 class="title is-4">
+          Let’s understand it further
+        </h2><br>
+        <p class="has-text-left">
+          Now, we have different checks that a fact checked article has to go through. Dega just brings all of those standards together so you don’t have to go to waste time in manually checking for them or going to different places for enforcing these standards including the ‘Claim Review’ schema.
+        </p><br>
+        <p class="has-text-left">
+          When you publish a fact-check article on Dega, the output is complaint with the ‘Claim Review’ schema as well as the standards of the IFCN.
+        </p><br>
       </div>
       <section class="container is-paddingless-top is-hidden-mobile" style="padding-bottom: 2rem;">
         <div class="tile is-ancestor">
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">How will Dega help me?</p>
+              <p class="title is-4">
+                How will Dega help me?
+              </p>
               <div class="content">
                 <p>Well, if you’re into Fact checking, then Dega is the platform to be! It makes the task of publishing fact checks simpler & more effective.</p>
               </div>
@@ -38,7 +46,9 @@
           </div>
           <div class="tile is-parent is-8 is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Why should you choose Dega?</p>
+              <p class="title is-4">
+                Why should you choose Dega?
+              </p>
               <div class="content">
                 <p>Going forward, Fact-Checking is going to become a mainstay of any newsroom. With all technology platforms moving towards using ‘Claim Review’ schema as a means to identify fact-check content, Dega helps newsrooms implement this seamlessly without any major changes to the existing technical architecture. </p>
               </div>
@@ -48,7 +58,9 @@
         <div class="tile is-ancestor has-text-centered">
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Who is Dega for? </p>
+              <p class="title is-4">
+                Who is Dega for?
+              </p>
               <div class="content">
                 <p>Dega is equally useful for individuals and organizations.</p>
               </div>
@@ -58,7 +70,9 @@
         <div class="tile is-ancestor">
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Individuals</p>
+              <p class="title is-4">
+                Individuals
+              </p>
               <div class="content">
                 <p>Individuals can sign up to be on Dega and publish fact check stories in your name just like you do on Medium. However, there will some form of editorial oversight (approval process) before your fact-check gets published. This is intended for individuals who wish to fact-check from across the world and in various languages. This we hope would serve the hyper local fact check needs.</p>
               </div>
@@ -66,7 +80,9 @@
           </div>
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Organizations</p>
+              <p class="title is-4">
+                Organizations
+              </p>
               <div class="content">
                 <p>Dega is available for organizations (small or big) and existing publishers. Since Dega is built on Micro-Service architecture, all the features are exposed as REST API for organizations that would like to build their own UI or integrate Dega with their existing interface. </p>
               </div>
@@ -76,7 +92,9 @@
         <div class="tile is-ancestor has-text-centered">
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Why should you choose Dega?</p>
+              <p class="title is-4">
+                Why should you choose Dega?
+              </p>
               <div class="content">
                 <p>Going forward, Fact-Checking is going to become a mainstay of any newsroom. With all technology platforms moving towards using ‘Claim Review’ schema as a means to identify fact-check content, Dega helps newsrooms implement this seamlessly without any major changes to the existing technical architecture. </p>
               </div>
@@ -88,7 +106,9 @@
         <div class="tile is-ancestor">
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">How will Dega help me?</p>
+              <p class="title is-4">
+                How will Dega help me?
+              </p>
               <div class="content">
                 <p>Well, if you’re into Fact checking, then Dega is the platform to be! It makes the task of publishing fact checks simpler & more effective.</p>
               </div>
@@ -96,7 +116,9 @@
           </div>
           <div class="tile is-parent is-8 is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Why should you choose Dega?</p>
+              <p class="title is-4">
+                Why should you choose Dega?
+              </p>
               <div class="content">
                 <p>Going forward, Fact-Checking is going to become a mainstay of any newsroom. With all technology platforms moving towards using ‘Claim Review’ schema as a means to identify fact-check content, Dega helps newsrooms implement this seamlessly without any major changes to the existing technical architecture. </p>
               </div>
@@ -104,7 +126,9 @@
           </div>
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Who is Dega for? </p>
+              <p class="title is-4">
+                Who is Dega for?
+              </p>
               <div class="content">
                 <p>Dega is equally useful for individuals and organizations.</p>
               </div>
@@ -112,7 +136,9 @@
           </div>
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Individuals</p>
+              <p class="title is-4">
+                Individuals
+              </p>
               <div class="content">
                 <p>Individuals can sign up to be on Dega and publish fact check stories in your name just like you do on Medium. However, there will some form of editorial oversight (approval process) before your fact-check gets published. This is intended for individuals who wish to fact-check from across the world and in various languages. This we hope would serve the hyper local fact check needs.</p>
               </div>
@@ -120,7 +146,9 @@
           </div>
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Organizations</p>
+              <p class="title is-4">
+                Organizations
+              </p>
               <div class="content">
                 <p>Dega is available for organizations (small or big) and existing publishers. Since Dega is built on Micro-Service architecture, all the features are exposed as REST API for organizations that would like to build their own UI or integrate Dega with their existing interface. </p>
               </div>
@@ -128,7 +156,9 @@
           </div>
           <div class="tile is-parent is-shady">
             <article class="tile is-child notification is-white">
-              <p class="title is-4">Why should you choose Dega?</p>
+              <p class="title is-4">
+                Why should you choose Dega?
+              </p>
               <div class="content">
                 <p>Going forward, Fact-Checking is going to become a mainstay of any newsroom. With all technology platforms moving towards using ‘Claim Review’ schema as a means to identify fact-check content, Dega helps newsrooms implement this seamlessly without any major changes to the existing technical architecture. </p>
               </div>
@@ -137,9 +167,7 @@
         </div>
       </section>
     </div>
-
   </div>
-
 </template>
 
 <script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet',
         href: 'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
         integrity: 'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr',
@@ -73,7 +73,7 @@ module.exports = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/,
           options: {
-            fix: false
+            fix: true
           }
         })
       }
