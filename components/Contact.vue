db0dbdeb61928da5ff040d32b24a8151cc8946eb
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-03-01 07:12:32 -0600

    Added a couple of line breaks on contact us page after the title

--- components/Contact.vue
+++ components/Contact.vue
@@ -6,6 +6,7 @@
           Contact Us
         </h3>
         <h4 class="subtitle is-5">
+          <br><br>
           We love to hear from you if you have any comments or if you want to give Dega a try for your fact-checking initiative. <br>
           Please write to us at hi@factly.in and someone from the team will reach out to you soon. Thanks!
         </h4>
