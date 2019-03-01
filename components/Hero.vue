1e65bf202a23946e33753db281461b15c7e0a399
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-03-01 05:06:18 -0600

    fixed the cover page on mobile

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,13 +1,28 @@
 <template>
-  <section class="hero is-fullheight is-default is-centered is-link is-cover-page">
-    <p class="has-text-right sticky" style="display: hidden">
-      <a href="/about" class="button is-medium is-warning is-inverted">
-        Learn more
-      </a>
-    </p>
+  <div>
+    <div class="is-hidden-mobile">
+      <section class="hero is-fullheight is-default is-centered is-link is-cover-page">
+        <p class="has-text-right sticky" style="display: hidden">
+          <a href="/about" class="button is-medium is-warning is-inverted">
+            Learn more
+          </a>
+        </p>
 
-    <div class="hero-body" />
-  </section>
+        <div class="hero-body" />
+      </section>
+    </div>
+    <div class="is-hidden-tablet">
+      <section class="hero is-fullheight is-default is-centered is-link is-mobile-cover-page">
+        <p class="has-text-right sticky" style="display: hidden">
+          <a href="/about" class="button is-medium is-warning is-inverted">
+            Learn more
+          </a>
+        </p>
+
+        <div class="hero-body" />
+      </section>
+    </div>
+  </div>
 </template>
   
 <script>
