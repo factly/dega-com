692baaa718bb0058a8ac7ab7f29f94d3fc85addc
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-03-01 07:16:47 -0600

    added a couple more breaks to format it correct on mobile for contactus

--- components/Contact.vue
+++ components/Contact.vue
@@ -6,8 +6,8 @@
           Contact Us
         </h3>
         <h4 class="subtitle is-5">
-          <br><br>
-          We love to hear from you if you have any comments or if you want to give Dega a try for your fact-checking initiative. <br>
+          <br><br><br>
+          We love to hear from you if you have any comments or if you want to give Dega a try for your fact-checking initiative. <br><br>
           Please write to us at hi@factly.in and someone from the team will reach out to you soon. Thanks!
         </h4>
       </div>
