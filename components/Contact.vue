4372eb52df1867fa2e1e5172da46dd9f02d51c09
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-03-01 06:38:58 -0600

    replaced the form with the netlify example

--- components/Contact.vue
+++ components/Contact.vue
@@ -11,54 +11,28 @@
       </div>
       <br>
     </div>
-
-    <form name="contactus" method="POST">
-      <div class="field">
-        <label class="label has-text-white">Name</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="full name" name="fullName">
-        </div>
-      </div>
-      <div class="field">
-        <label class="label has-text-white">Organisation</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="organisation name" name="organisation">
-        </div>
-      </div>
-      <div class="field">
-        <label class="label has-text-white">Email</label>
-        <div class="control has-icons-left">
-          <input class="input" type="email" placeholder="valid email address" name="email">
-          <span class="icon is-small is-left">
-            <i class="fas fa-envelope" />
-          </span>
-        </div>
-      </div>
-      <div class="field">
-        <label class="label has-text-white">Phone</label>
-        <div class="control has-icons-left">
-          <input class="input" type="text" placeholder="include the country code" name="phone">
-          <span class="icon is-small is-left">
-            <i class="fas fa-phone" />
-          </span>
-        </div>
-      </div>
-      <div class="field has-text-white">
-        <label class="label has-text-white">Message</label>
-        <div class="control">
-          <textarea class="textarea" placeholder="please be as descriptive as possible" name="message" />
-        </div>
-      </div>
-      <div class="field is-grouped has-text-centered">
-        <div class="control">
-          <button class="button is-warning is-medium" type="submit">
-            <span class="icon">
-              <i class="fas fa-envelope" />
-            </span>
-            <span>Submit</span>
-          </button>
-        </div>
-      </div>
+    
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Name: <input type="text" name="name"></label>
+      </p>
+      <p>
+        <label>Organisation: <input type="text" name="organisation"></label>
+      </p>
+      <p>
+        <label>Email: <input type="email" name="email"></label>
+      </p>
+      <p>
+        <label>Phone: <input type="text" name="phone"></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message" /></label>
+      </p>
+      <p>
+        <button type="submit">
+          Send
+        </button>
+      </p>
     </form>
   </section>
 </template>
